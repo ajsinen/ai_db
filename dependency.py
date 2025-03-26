9da7209a5f968645126590620c36df4a0f6b48f3
ASin <sinenaldous@gmail.com>
2025-03-26 22:19:16 +0800

    update ui and database query

---
+++ dependency.py
@@ -0,0 +1,5 @@
+header = "Your Smart SQL Query Assistant"
+
+brief_intro = ("Tired of writing SQL? Just ask **raymondAI!** This smart AI assistant turns your banking questions into SQL "
+               "queries—no manual coding needed. Whether you're hunting down transactions or analyzing customer trends, "
+               "BankAI's got your back.")
---
+++ ollama_api.py
@@ -0,0 +1,4 @@
+ollama_url = {
+    "generate": "http://localhost:11434/api/generate",
+    "chat": "http://localhost:11434/api/chat",
+}
